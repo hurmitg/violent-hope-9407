c5039bcebbc9eb626aa1f377d0c3643a3ec83e1b
SHARUN266 <sharunattari266@gmail.com>
2022-11-10 16:36:19 +0530

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Layout from './Landing Page/Layout';
+import LoginMain from './Login/Main';
 
 function App() {
   return (
     <>
-    <Layout/>
-    
+    <LoginMain/>
     </>
   );
 }
