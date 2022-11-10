948c4936e24b16e366eea976828d057ccb217a84
SHARUN266 <sharunattari266@gmail.com>
2022-11-10 16:26:08 +0530

    Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
+import "./App.css";
 
-import './App.css';
+import Layout from "./Landing Page/Layout";
 
-import Layout from './Landing Page/Layout';
-
-import Kids from './Components/Kids';
-import Men from './Components/Men';
-import Women from './Components/Women';
-
+import Kids from "./Components/Kids";
+import Men from "./Components/Men";
+import Women from "./Components/Women";
+import LoginMain from "./Login/Main";
 
 function App() {
   return (
     <>
-
-    <Layout/>
-
-    <Women/>
-    <Men/>
-    <Kids/>
-
-    
+      <LoginMain/>
     </>
   );
 }
