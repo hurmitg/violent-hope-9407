bd532d54f84f5a081c9e2506b47b57bd1276eaad
SHARUN266 <sharunattari266@gmail.com>
2022-11-12 23:03:51 +0530

    footer

--- frontend/src/Footer/Footer.jsx
+++ frontend/src/Footer/Footer.jsx
@@ -13,11 +13,12 @@ import {
   useColorModeValue,
   Button,
 } from "@chakra-ui/react";
-import { ReactNode } from "react";
+import { ReactNode, useContext } from "react";
 import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 import { BiMailSend, BiMobile } from "react-icons/bi";
 import { BsLinkedin, BsPinterest } from "react-icons/bs";
 import Logo from "../Components/Logo";
+import { AppContext } from "../Context/Context";
 
 
 
@@ -54,6 +55,11 @@ const ListHeader = ({ children }) => {
 };
 
 export default function LargeWithNewsletter() {
+  const {nav}=useContext(AppContext);
+  function handleLogin() {
+    window.scroll({top:0,left:0})
+    nav("/login")
+  }
   return (
     <Box
       w="90%"
@@ -94,10 +100,12 @@ export default function LargeWithNewsletter() {
             <ListHeader color="#111">GET TREND UPDATES, STYLE TIPS AND MORE</ListHeader>
             <Stack direction={"row"}>
               <Input
+              onClick={handleLogin}
                 placeholder={"Your email address here..."}
                 borderRadius={"none"}
               />
               <Button
+               onClick={handleLogin}
                 p="0rem 2rem"
                 bg={"#f2f2f2"}
                 fontWeight={100}
