ce4fff8eed1eae24b0493e48a383f23b27ba2a61
Vivek Nemade <101920079+VivekN11@users.noreply.github.com>
2022-11-12 18:06:18 +0530

    day-5
    All API Added
    Functioning Properly

--- backend/models/cart.model.js
+++ backend/models/cart.model.js
@@ -8,17 +8,13 @@ const cartSchema = mongoose.Schema(
       ref: "User",
     },
     cartItems: [
-      {
-        name: { type: String, required: true },
-        quantity: { type: String, required: true },
-        image: { type: String, required: true },
-        price: { type: String, required: true },
-        products: {
-          type: mongoose.Schema.Types.ObjectId,
-          required: true,
-          ref: "Product",
-        },
-      },
+          {
+            product:{ 
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "Product",
+            },
+            quantity: {type :Number, required:true}
+          }  
     ],
   },
   {
